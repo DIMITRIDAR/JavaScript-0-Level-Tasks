0747227cefba95cce2dca3c439f7c6a6bace8235
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-01-24 01:14:41 +0400

    Write down 3 task

--- app.js
+++ app.js
@@ -8,6 +8,19 @@
 //Task 2
 //In this task we have to create a variable and assign a number to it. Print the value of the variable.
 
-let x = 5;
+// let x = 5;
 
-console.log(x);
+// console.log(x);
+
+//Task 3
+//In this task we have to write a program to sum two numbers and print the result.
+
+//First way
+
+// console.log(5 + 6);
+
+//Second way
+
+// let x = 5;
+// let y = 6;
+// console.log(x + y);
