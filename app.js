2feb6c1170ceb44336597f137270cb00e40b7177
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-01-24 01:12:00 +0400

    Write down 2 task

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 //Chapter 1
+
 //Task 1
-//In this task we have to write a program which will console "Hello World"
+//In this task we have to write a program which will console "Hello World".
 
 //console.log("Hello World");
+
+//Task 2
+//In this task we have to create a variable and assign a number to it. Print the value of the variable.
+
+let x = 5;
+
+console.log(x);
