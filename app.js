922e3dbd0d9ee192e9cb8a311351f6ed31d942ba
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-01-30 01:37:15 +0400

    Write down Chapter 3 Task 5

--- app.js
+++ app.js
@@ -227,3 +227,11 @@
 // }
 
 //Task 5
+//In this task we have to write a program that calculates the sum of numbers, from 1 to 100 using a for loop
+//and prints the result
+
+// let sum = 0;
+// for (let i = 1; i < 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
