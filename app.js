b38b8ed29d20370ab747ab29c1d4b64dc736159d
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-01-31 14:49:22 +0400

    Write down Chapter 4 Task 1

--- app.js
+++ app.js
@@ -348,3 +348,17 @@
 // }
 
 // console.log(countBs("BoBBy's birthday"));
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//Chapter 4
+
+//Task 1
+//In this task we have to write a program to find the sum of all the numbers in an array.
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum);
