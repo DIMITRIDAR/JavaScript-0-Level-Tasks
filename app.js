b9f8a3fefa29a3163987abfaa2839d554e154a6b
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-02-02 15:20:33 +0400

    Write down Chapter 4 Task 9

--- app.js
+++ app.js
@@ -446,3 +446,17 @@
 // }
 
 // console.log(count);
+
+//Task9
+//In this task we have to write a program to find the sum of all even numbers in an array.
+
+// // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let sum = 0;
+
+// for (i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     sum += arr[i];
+//   }
+// }
+
+// console.log(sum);
